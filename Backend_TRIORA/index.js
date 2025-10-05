f5343c4d1ead9be5a6321813d13aa60345e1deff
MANASIJDAS <manasijdas22@gmail.com>
2025-10-05 12:58:15 +0530

    Sace changes

--- Backend_TRIORA/index.js
+++ Backend_TRIORA/index.js
@@ -3,25 +3,30 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoute from "./Route/User_Route.js";
 import cors from "cors";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 dotenv.config();
-const PORT=process.env.PORT || 4000;
-const URI=process.env.MongoDBURI;
-//connect to MongoDB
-try{
-    mongoose.connect(URI,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true
+
+const PORT = process.env.PORT || 4000;
+const URI = process.env.MongoDBURI;
+
+// Connect to MongoDB
+try {
+    mongoose.connect(URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     });
     console.log("Connected to MongoDB.");
+} catch (error) {
+    console.log("Error occurred is", error);
 }
-catch(error){
-    console.log("Error occurred is ",error);
-}
-//defining routes
-app.use("/user",userRoute)
+
+// Define routes
+app.use("/user", userRoute);
+
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-})
+    console.log(`Server is listening on port ${PORT}`);
+});
